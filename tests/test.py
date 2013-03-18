c2f1ded93f7921b97ddcab7e5129eb7a52af529f
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-03-19 00:07:44 +0200

    Added tests for Vehicle

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_given_example(self):
+
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_vehicle.py
@@ -0,0 +1,49 @@
+import unittest
+
+from kivy.vector import Vector
+from rushsimulation.vehicle import Vehicle
+
+
+class TestVehicle(unittest.TestCase):
+    def setUp(self):
+        self.vehicle = Vehicle()
+
+    def test_is_in_target(self):
+        self.vehicle.target = Vector(100, 100)
+        self.vehicle.pos = Vector(100, 100)
+        self.assertTrue(self.vehicle.is_in_target())
+        self.vehicle.pos = Vector(
+            100 - self.vehicle.TARGET_OFFSET,
+            100 - self.vehicle.TARGET_OFFSET
+        )
+        self.assertTrue(self.vehicle.is_in_target())
+        self.vehicle.pos = Vector(
+            100 + self.vehicle.TARGET_OFFSET,
+            100 + self.vehicle.TARGET_OFFSET
+        )
+        self.assertTrue(self.vehicle.is_in_target())
+
+    def test_is_not_in_target(self):
+        self.vehicle.target = Vector(100, 100)
+        self.vehicle.pos = Vector(0, 0)
+        self.assertFalse(self.vehicle.is_in_target())
+        self.vehicle.pos = Vector(
+            100 - self.vehicle.TARGET_OFFSET - 1,
+            100 - self.vehicle.TARGET_OFFSET
+        )
+        self.assertFalse(self.vehicle.is_in_target())
+        self.vehicle.pos = Vector(
+            100 + self.vehicle.TARGET_OFFSET,
+            100 + self.vehicle.TARGET_OFFSET + 1
+        )
+        self.assertFalse(self.vehicle.is_in_target())
+
+    def test_set_to_start_position(self):
+        self.vehicle.set_to_start_position()
+        self.assertEqual(self.vehicle.y, 0)
+        self.assertTrue(self.vehicle.x >= 0)
+        self.assertTrue(self.vehicle.x < 800)
+
+
+if __name__ == '__main__':
+    unittest.main()
