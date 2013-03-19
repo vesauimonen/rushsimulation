bd55a04e11caf249a3e8ddf547c615cc1d0ea847
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-03-19 11:18:14 +0200

    Added truncate method to Vector

---
+++ rushsimulation/vector.py
@@ -0,0 +1,19 @@
+from kivy.vector import Vector as BaseVector
+
+
+class Vector(BaseVector):
+    """
+    Extends Kivy's Vector class with a truncate method.
+    """
+    def __init__(self, *largs):
+        super(Vector, self).__init__(*largs)
+
+    def truncate(self, limit):
+        '''
+        Truncate vector by limit.
+
+        :param limit: max length of the returned vector
+        '''
+        if self.length() > limit:
+            return self.normalize() * limit
+        return self
