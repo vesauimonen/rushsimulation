7e5deeeddd03ca23a580db2d26e3672612e2203d
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-04-16 14:05:27 +0300

    Added configs to a JSON file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 if __name__ == '__main__':
+    import json
     from rushsimulation.simulation import RushApp
-    RushApp().run()
+    from rushsimulation.config_handler import ConfigHandler
+    config_handler = ConfigHandler(json.load(open('config.json')))
+    user_configs = config_handler.handle()
+    RushApp(user_configs=user_configs).run()
