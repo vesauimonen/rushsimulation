5a86338723011e647c61c4cba44636d25d4d8c2d
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-04-16 14:08:10 +0300

    Added braking behaviour and obstacle avoider Vector

--- rushsimulation/vehicle.py
+++ rushsimulation/vehicle.py
@@ -2,34 +2,37 @@ from random import randint
 
 from kivy.uix.widget import Widget
 from kivy.utils import interpolate
+
 from .vector import Vector
 
 
 class Vehicle(Widget):
     TARGET_OFFSET = 10
-    SEPARATION_WEIGHT = 10
+    NEARBY_OFFSET = 50
+    IN_FRONT_OF_OFFSET = 50
+    IN_FRONT_OF_ANGLE = 120
+    OBSTACLE_AVOIDANCE_ANGLE = 70
+    SEPARATION_WEIGHT = 100
+    SEEKING_WEIGHT = 1
+    BRAKING_RATE = 5.25
     velocity = Vector(0, 0)
     acceleration = Vector(0, 0)
     steering_force = Vector(0, 0)
-    max_force = 7
-    max_speed = 4
-    radius = 8
-    mass = 10
+    obstacle_avoider = Vector(0, 0)
 
-    def __init__(self, target_vector=Vector(400, 600), *args, **kwargs):
+    def __init__(self, user_configs, target_vector, *args, **kwargs):
         super(Vehicle, self).__init__(*args, **kwargs)
+        self.user_configs = user_configs
+        self.max_speed = self.user_configs['Vehicle']['maxSpeed']
+        self.max_force = self.user_configs['Vehicle']['maxForce']
+        self.mass = self.user_configs['Vehicle']['mass']
+        self.diameter = self.user_configs['Vehicle']['diameter']
         self.target = target_vector
-
-    def move(self):
-        self.steering_force = self.get_overall_steering_force()
-        self.acceleration = self.steering_force / self.mass
-        self.velocity = self.velocity + self.acceleration
-        self.velocity = Vector(*self.velocity).truncate(self.max_speed)
-        self.pos = interpolate(self.pos, self.velocity + self.pos, 2)
+        self.size = (self.diameter, self.diameter)
 
     def set_to_start_position(self):
         self.pos = Vector(randint(0, 800), 0)
-        self.max_speed = randint(3, 5)
+        self.max_speed = randint(3, 8)
         self.velocity = Vector(0, 0)
         self.acceleration = Vector(0, 0)
         self.steering_force = Vector(0, 0)
@@ -40,27 +43,46 @@ class Vehicle(Widget):
             self.y <= self.target.y + self.TARGET_OFFSET and\
             self.y >= self.target.y - self.TARGET_OFFSET
 
+    def move(self, dt):
+        self.velocity = self.calculate_new_velocity(dt)
+        self.obstacle_avoider = self.calculate_new_obstacle_avoider()
+        self.pos = interpolate(self.pos, self.velocity + self.pos, 2)
+
+    def calculate_new_velocity(self, dt):
+        distance = self.get_distance_to_slow_vehicle()
+        if distance < self.IN_FRONT_OF_OFFSET:
+            return self.brake(distance, dt)
+        else:
+            self.steering_force = self.get_overall_steering_force()
+            self.acceleration = self.steering_force / self.mass
+            velocity = self.velocity + self.acceleration
+            velocity = Vector(velocity).truncate(self.max_speed)
+            return velocity
+
+    def calculate_new_obstacle_avoider(self):
+        return (self.velocity.normalize().rotate(randint(
+            -self.OBSTACLE_AVOIDANCE_ANGLE / 2,
+            self.OBSTACLE_AVOIDANCE_ANGLE / 2
+        )) * 20)
+
+    def brake(self, distance, dt):
+        if distance <= self.diameter + 4:
+            return Vector(0, 0)
+        speed = self.velocity.length()
+        raw_braking = speed * self.BRAKING_RATE
+        raw_braking *= (self.IN_FRONT_OF_OFFSET / distance)
+        if raw_braking < self.max_force:
+            clip_braking = raw_braking
+        else:
+            clip_braking = self.max_force
+
+        return self.velocity.normalize() * (speed - (clip_braking * dt))
+
     def get_overall_steering_force(self):
-        # if self.get_braking_force() != Vector(0, 0):
-        #     return self.get_braking_force()
-        return 0.5 * self.get_seeking_force() + self.get_separation_force()
-
-    def get_nearby_vehicles(self):
-        nearby_vehicles = []
-        for vehicle in self.parent.vehicles:
-            distance = Vector(self.pos).distance(vehicle.pos)
-            if vehicle is not self and distance <= 50:
-                nearby_vehicles.append(vehicle)
-        return nearby_vehicles
-
-    def get_vehicles_in_front_of(self):
-        vehicles_in_front_of = []
-        for vehicle in self.parent.vehicles:
-            direction = -Vector(self.pos) + Vector(vehicle.pos)
-            angle = self.velocity.angle(direction)
-            if vehicle is not self and direction.length() <= 50 and angle > -90 and angle < 90:
-                vehicles_in_front_of.append(vehicle)
-        return vehicles_in_front_of
+        return (
+            self.get_seeking_force() * self.SEEKING_WEIGHT +
+            self.get_separation_force() * self.SEPARATION_WEIGHT
+        )
 
     def get_seeking_force(self):
         '''
@@ -76,11 +98,14 @@ class Vehicle(Widget):
 
     def get_separation_force(self):
         sum_force = Vector(0, 0)
-        for vehicle in self.get_nearby_vehicles():
+        nearby_vehicles = self.get_nearby_vehicles(
+            self.NEARBY_OFFSET
+        )
+        for vehicle in nearby_vehicles:
             direction = Vector(self.pos) - Vector(vehicle.pos)
             distance = Vector(self.pos).distance(vehicle.pos)
             if distance != 0:
-                sum_force = sum_force + direction * (1 / distance ** 2) * self.SEPARATION_WEIGHT
+                sum_force = sum_force + direction * (1 / distance ** 2)
         sum_force = Vector(*sum_force).truncate(self.max_force)
         return sum_force
 
@@ -88,8 +113,41 @@ class Vehicle(Widget):
         # Todo
         pass
 
-    def get_braking_force(self):
-        for vehicle in self.get_vehicles_in_front_of():
-            if vehicle.velocity.length() < self.velocity.length():
-                return -Vector(self.steering_force)
-        return Vector(0, 0)
+    def get_distance_to_slow_vehicle(self):
+        vehicles_in_front_of = self.get_vehicles_in_front_of(
+            self.IN_FRONT_OF_OFFSET,
+            self.IN_FRONT_OF_ANGLE
+        )
+        smallest_distance = self.IN_FRONT_OF_OFFSET
+        for vehicle in vehicles_in_front_of:
+            distance = Vector(self.pos).distance(vehicle.pos)
+            speed = vehicle.velocity.length()
+            if (speed < self.velocity.length() and
+                    distance < smallest_distance):
+                smallest_distance = distance
+        return smallest_distance
+
+    def get_nearby_vehicles(self, boundary):
+        nearby_vehicles = []
+        for vehicle in self.parent.vehicles:
+            distance = Vector(self.pos).distance(vehicle.pos)
+            if vehicle is not self and distance <= boundary:
+                nearby_vehicles.append(vehicle)
+        return nearby_vehicles
+
+    def get_vehicles_in_front_of(self, boundary, angle_boundary):
+        vehicles_in_front_of = []
+        for vehicle in self.parent.vehicles:
+            direction = -Vector(self.pos) + Vector(vehicle.pos)
+            if vehicle is self or direction.length() > boundary:
+                continue
+            within_angle = self.vectors_are_within_angular_range(
+                self.velocity, direction, angle_boundary
+            )
+            if within_angle:
+                vehicles_in_front_of.append(vehicle)
+        return vehicles_in_front_of
+
+    def vectors_are_within_angular_range(self, vector_1, vector_2, angle):
+        real_angle = vector_1.angle(vector_2)
+        return real_angle > -angle / 2 and real_angle < angle / 2
