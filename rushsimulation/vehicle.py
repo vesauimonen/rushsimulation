4f6733890d1e43b3c1dd528ab23d90f0fae12efc
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-04-17 01:26:49 +0300

    Implemented first working proto of wall avoiding

--- rushsimulation/vehicle.py
+++ rushsimulation/vehicle.py
@@ -1,5 +1,6 @@
 from random import randint
 
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.utils import interpolate
 
@@ -11,14 +12,16 @@ class Vehicle(Widget):
     NEARBY_OFFSET = 50
     IN_FRONT_OF_OFFSET = 50
     IN_FRONT_OF_ANGLE = 120
-    OBSTACLE_AVOIDANCE_ANGLE = 70
+    WALL_AVOIDANCE_ANGLE = 70
+    WALL_AVOIDANCE_DISTANCE = 120
     SEPARATION_WEIGHT = 100
     SEEKING_WEIGHT = 1
     BRAKING_RATE = 5.25
     velocity = Vector(0, 0)
     acceleration = Vector(0, 0)
     steering_force = Vector(0, 0)
-    obstacle_avoider = Vector(0, 0)
+    wall_lookup_point = Vector(0, 0)
+    collision = Vector(0, 0)
 
     def __init__(self, user_configs, target_vector, *args, **kwargs):
         super(Vehicle, self).__init__(*args, **kwargs)
@@ -38,17 +41,34 @@ class Vehicle(Widget):
         self.steering_force = Vector(0, 0)
 
     def is_in_target(self):
-        return self.x <= self.target.x + self.TARGET_OFFSET and\
+        '''
+        Determines if the vehicle has reached its target or is outside the top
+        of the window.
+        '''
+        within_offset = self.x <= self.target.x + self.TARGET_OFFSET and\
             self.x >= self.target.x - self.TARGET_OFFSET and\
             self.y <= self.target.y + self.TARGET_OFFSET and\
             self.y >= self.target.y - self.TARGET_OFFSET
+        out_of_top = self.top > Window.height
+        if within_offset or out_of_top:
+            return True
+        return False
 
     def move(self, dt):
+        '''
+        Moves the vehicle according to Simple Vehicle Model principles.
+        This method is called by self.parent repeatedly.
+        '''
+        self.wall_lookup_point = self.calculate_new_wall_lookup_point()
         self.velocity = self.calculate_new_velocity(dt)
-        self.obstacle_avoider = self.calculate_new_obstacle_avoider()
         self.pos = interpolate(self.pos, self.velocity + self.pos, 2)
 
     def calculate_new_velocity(self, dt):
+        '''
+        Calculates a new velocity for the vehicle. If slower vehicles are
+        nearby, braking occurs. Otherwise steering force is used for
+        the new velocity.
+        '''
         distance = self.get_distance_to_slow_vehicle()
         if distance < self.IN_FRONT_OF_OFFSET:
             return self.brake(distance, dt)
@@ -59,14 +79,27 @@ class Vehicle(Widget):
             velocity = Vector(velocity).truncate(self.max_speed)
             return velocity
 
-    def calculate_new_obstacle_avoider(self):
-        return (self.velocity.normalize().rotate(randint(
-            -self.OBSTACLE_AVOIDANCE_ANGLE / 2,
-            self.OBSTACLE_AVOIDANCE_ANGLE / 2
-        )) * 20)
+    def calculate_new_wall_lookup_point(self):
+        '''
+        Returns a random point in front of the vehicle within a certain angle
+        and distance.
+        '''
+        offset = self.velocity.normalize()
+        offset = offset.rotate(randint(
+            -self.WALL_AVOIDANCE_ANGLE / 2,
+            self.WALL_AVOIDANCE_ANGLE / 2
+        ))
+        offset *= self.WALL_AVOIDANCE_DISTANCE
+        return (self.pos[0] + offset[0], self.pos[1] + offset[1])
 
     def brake(self, distance, dt):
+        '''
+        Returns a velocity less than current velocity, thus simulating
+        a braking behaviour. The closer the nearby vehicle is the greater the
+        decrease of the velocity is.
+        '''
         if distance <= self.diameter + 4:
+            # Very close to another vehicle, full brakes.
             return Vector(0, 0)
         speed = self.velocity.length()
         raw_braking = speed * self.BRAKING_RATE
@@ -75,10 +108,26 @@ class Vehicle(Widget):
             clip_braking = raw_braking
         else:
             clip_braking = self.max_force
-
         return self.velocity.normalize() * (speed - (clip_braking * dt))
 
     def get_overall_steering_force(self):
+        '''
+        Returns an overall force which simulates different behaviours of the
+        Simple Vehicle Model. Wall avoidance is prioritized over other
+        behaviours.
+        '''
+        wall_1_collision = self.parent.wall_1.get_collision_point(
+            self.wall_lookup_point,
+            self.pos
+        )
+        wall_2_collision = self.parent.wall_2.get_collision_point(
+            self.wall_lookup_point,
+            self.pos
+        )
+        if wall_1_collision:
+            return Vector(1, 0).normalize() * self.max_force
+        if wall_2_collision:
+            return Vector(-1, 0).normalize() * self.max_force
         return (
             self.get_seeking_force() * self.SEEKING_WEIGHT +
             self.get_separation_force() * self.SEPARATION_WEIGHT
@@ -86,21 +135,24 @@ class Vehicle(Widget):
 
     def get_seeking_force(self):
         '''
-        Calculates the force related to the seeking behaviour of the vehicle.
+        Calculates the force related to the seeking behaviour of the Simple
+        Vehicle Model.
         '''
         # Vector from vehicle position towards target
-        desired_velocity = Vector(
+        desired_direction = Vector(
             self.target[0] - self.pos[0],
             self.target[1] - self.pos[1]
         )
-        desired_force = desired_velocity.truncate(self.max_force)
-        return self.velocity + desired_force
+        force = desired_direction.truncate(self.max_force)
+        return self.velocity + force
 
     def get_separation_force(self):
+        '''
+        Calculates the force related to the separation behaviour of the Simple
+        Vehicle Model.
+        '''
         sum_force = Vector(0, 0)
-        nearby_vehicles = self.get_nearby_vehicles(
-            self.NEARBY_OFFSET
-        )
+        nearby_vehicles = self.get_nearby_vehicles(self.NEARBY_OFFSET)
         for vehicle in nearby_vehicles:
             direction = Vector(self.pos) - Vector(vehicle.pos)
             distance = Vector(self.pos).distance(vehicle.pos)
--- rushsimulation/wall.py
+++ rushsimulation/wall.py
@@ -1,5 +1,7 @@
 from kivy.uix.widget import Widget
 
+from .vector import Vector
+
 
 class Wall(Widget):
 
@@ -7,3 +9,43 @@ class Wall(Widget):
         super(Wall, self).__init__(*args, **kwargs)
         self.pos = pos
         self.size = size
+
+    def get_collision_point(self, tip, pos):
+        '''
+        Calculates a point in the wall edge which is between points tip and
+        pos.
+        '''
+        y = self.y
+        if (pos[1] - tip[1]) != 0:
+            inverse_k = float(pos[0] - tip[0]) / float(pos[1] - tip[1])
+            x = inverse_k * (y - pos[1]) + pos[0]
+        else:
+            x = pos[0]
+        point = Vector(x, y)
+        if self.is_inside(point) and self.is_between_points(pos, tip, point):
+            return point
+        return None
+
+    def is_between_points(self, a, b, c):
+        '''
+        Determines if point c is between points a and b. Assumes that all the
+        points are aligned.
+        '''
+        # Dot product of (b-a) and (c-a)
+        dot = (c[0] - a[0]) * (b[0] - a[0]) + (c[1] - a[1]) * (b[1] - a[1])
+        if dot < 0:
+            return False
+        # The square of the distance between a and b
+        squared_length = (b[0] - a[0]) * (b[0] - a[0])
+        squared_length += (b[1] - a[1]) * (b[1] - a[1])
+        if dot > squared_length:
+            return False
+        return True
+
+    def is_inside(self, point):
+        '''
+        Determines if the point is within the wall.
+        '''
+        inside_x = point[0] >= self.x and point[0] <= self.x + self.width
+        inside_y = point[1] >= self.y and point[1] <= self.y + self.height
+        return inside_x and inside_y
