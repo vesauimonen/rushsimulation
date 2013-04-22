6106dcc4f6c83b6a0b9bf7fb3b24dd9477481c44
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-04-23 00:38:13 +0300

    Fixed JSON config loading in Windows

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     import json
     from rushsimulation.simulation import RushApp
     from rushsimulation.config_handler import ConfigHandler
-    path = os.path.join(os.path.curdir, 'config.json')
+    path = os.path.join(os.path.dirname(__file__), 'config.json')
     config_handler = ConfigHandler(json.load(open(path)))
     user_configs = config_handler.handle()
     RushApp(user_configs=user_configs).run()
