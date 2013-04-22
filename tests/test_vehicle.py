2c423391cfbba50b90ed00d7191fd7a99806275f
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-04-23 00:06:21 +0300

    Fixed some vehicle tests

--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -1,19 +1,14 @@
 import unittest
 
+from kivy.core.window import Window
+
 from rushsimulation.vector import Vector
-from rushsimulation.simulation import RushSimulation
 from rushsimulation.vehicle import Vehicle
 
+from .test_base import TestBase
 
-class TestVehicle(unittest.TestCase):
-    def setUp(self):
-        self.simulation = RushSimulation()
-        self.vehicle = Vehicle()
-        self.simulation.add_vehicle(self.vehicle)
 
-    def tearDown(self):
-        self.simulation.remove_vehicle(self.vehicle)
-        self.simulation = None
+class TestVehicle(TestBase):
 
     def test_is_in_target(self):
         self.vehicle.target = Vector(100, 100)
@@ -27,6 +22,11 @@ class TestVehicle(unittest.TestCase):
         self.vehicle.pos = Vector(
             100 + self.vehicle.TARGET_OFFSET,
             100 + self.vehicle.TARGET_OFFSET
+        )
+        self.assertTrue(self.vehicle.is_in_target())
+        self.vehicle.pos = Vector(
+            300,
+            Window.height + 2
         )
         self.assertTrue(self.vehicle.is_in_target())
 
@@ -47,32 +47,46 @@ class TestVehicle(unittest.TestCase):
 
     def test_set_to_start_position(self):
         self.vehicle.set_to_start_position()
-        self.assertEqual(self.vehicle.y, 0)
-        self.assertTrue(self.vehicle.x >= 0)
-        self.assertTrue(self.vehicle.x < 800)
-        self.assertEqual(self.vehicle.velocity, [0, 0])
-        self.assertEqual(self.vehicle.acceleration, [0, 0])
-        self.assertEqual(self.vehicle.steering_force, [0, 0])
+        self.assertTrue(self.vehicle.max_speed >= 4)
+        self.assertTrue(
+            self.vehicle.max_speed <= self.user_configs['Vehicle']['maxSpeed']
+        )
 
     def test_get_nearby_vehicles(self):
-        self.assertEqual(self.vehicle.get_nearby_vehicles(), [])
-        another_vehicle = Vehicle()
+        self.assertEqual(self.vehicle.get_nearby_vehicles(50), [])
+        another_vehicle = Vehicle(
+            self.user_configs,
+            target_vector=Vector(400, 600)
+        )
         self.simulation.add_vehicle(another_vehicle)
         self.assertEqual(
-            self.vehicle.get_nearby_vehicles(),
+            self.vehicle.get_nearby_vehicles(50),
             [another_vehicle]
         )
         self.simulation.remove_vehicle(another_vehicle)
 
+    def test_validate_pos(self):
+        self.vehicle.pos = (0, 0)
+        another_vehicle = Vehicle(
+            self.user_configs,
+            target_vector=Vector(400, 600)
+        )
+        self.simulation.add_vehicle(another_vehicle)
+        another_vehicle.pos = (0, 25)
+        self.assertEqual(another_vehicle.validate_pos((0, 3)), Vector(0, 15))
+
     def test_get_separation_force(self):
-        self.vehicle.pos = Vector(10, 10)
+        self.vehicle.pos = Vector(10, 30)
         self.assertEqual(self.vehicle.get_separation_force(), Vector(0, 0))
-        another_vehicle = Vehicle()
-        another_vehicle.pos = Vector(6, 7)
+        another_vehicle = Vehicle(
+            self.user_configs,
+            target_vector=Vector(400, 600)
+        )
+        another_vehicle.pos = Vector(10, 10)
         self.simulation.add_vehicle(another_vehicle)
         self.assertEqual(
             self.vehicle.get_separation_force(),
-            Vector(4, 3)
+            Vector(0, 0.05)
         )
         self.simulation.remove_vehicle(another_vehicle)
 
